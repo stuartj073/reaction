66e1bacb8fa96c40fc885a9f278617aaac221f2c
stuartj073 <stuartj073@gmail.com>
2022-03-14 10:53:23 +0000

    Create css file

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ window.addEventListener('DomContentLoaded', (event)=>{
     console.log("Dom Loaded");
 })
 
-const button = document.getElementById('load') = load
+const button = document.getElementById('load');
 
 function clickMe() {
     button.addEventListener('click', function(){
-        alert('Hi');
+        console.log('Hi');
     });
 }
