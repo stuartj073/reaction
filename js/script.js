0bc134c84216dd068e4226923ef52aec3ac0b822
stuartj073 <stuartj073@gmail.com>
2022-03-14 10:34:51 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.addEventListener('DomContentLoaded', (event)=>{
+    console.log("Dom Loaded");
+})
+
+const button = document.getElementById('load') = load
+
+function clickMe() {
+    button.addEventListener('click', function(){
+        alert('Hi');
+    });
+}
